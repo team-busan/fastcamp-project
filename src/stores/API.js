54ceadfa4d0b1182879f0e9a0cddf78fd57a88be
hongchun <jhc5317@gmail.com>
2024-01-17 14:35:05 +0900

    태그별 자세히보기 기본 틀, axiosMockData 기본 작업

---
+++ src/stores/API.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+import AxiosMockAdapter from "axios-mock-adapter";
+import { RESTAURANTS } from "./mockData";
+
+const BASE_URL = "https://localhost:8000/api/";
+
+const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  timeout: 5000,
+});
+
+const API_URL = {
+  TAGDETAIL: "tagdetail",
+};
+
+const axiosMock = new AxiosMockAdapter(axiosInstance, {
+  delayResponse: 100,
+  onNoMatch: "throwException",
+});
+
+axiosMock.onGet(API_URL.TAGDETAIL).reply(200, RESTAURANTS);
+
+export { axiosInstance, API_URL };
