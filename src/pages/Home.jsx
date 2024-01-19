8f234cf4c1f857e8a94f7cbf50851e48b4beee16
HongchunJung <jhc5317@gmail.com>
2024-01-19 22:35:55 +0900

    Revert "Feature home"

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,10 @@
-import Column from "../component/Column";
 import CustomBottomSheet from "../component/CustomBottomSheet";
-import HomeTagList from "../component/HomeTagList";
-import LocalFoodPick from "../component/LocalFoodPick";
-import Navbar from "../component/Navbar";
-// import Search from "../component/Search";
-import MbLocalFood from "../component/mbLocalFood";
-
 
 function Home() {
   return (
     <div>
-      <Navbar />
-      <LocalFoodPick />
-      <MbLocalFood />
-      <Column />
-      <HomeTagList />
+      <h1>Home</h1>
       <CustomBottomSheet />
-      
-      
     </div>
   );
 }
