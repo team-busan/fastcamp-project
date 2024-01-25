31428555412666b6c19299da53329907ebb52341
hongchun <jhc5317@gmail.com>
2024-01-25 16:51:44 +0900

    가게 상세페이지 네이버 지도 추가, 크기 조정

--- src/component/MbTitle.jsx
+++ src/component/MbTitle.jsx
@@ -1,62 +1,57 @@
 import { useEffect, useState } from "react";
 import { FaRegShareFromSquare } from "react-icons/fa6";
-import { FaStar} from "react-icons/fa";
-import { CiBookmark,CiLocationOn, CiFaceFrown   } from "react-icons/ci";
+import { FaStar } from "react-icons/fa";
+import { CiBookmark, CiLocationOn, CiFaceFrown } from "react-icons/ci";
 import { PiNotePencil } from "react-icons/pi";
 import { RiShareForward2Fill } from "react-icons/ri";
 
-
-export default function MbTitle({restaurant, review, averageRating}) {
-
+export default function MbTitle({ restaurant, review, averageRating }) {
   return (
-      <header className = "flex-col bg-white mb-3">
-        <div className = "flex">
-          <div>
-            <img 
-              src = {restaurant.imgLink}
-              alt = "error"
-              className = "h-72 w-screen"
-              />
-            <div className = "p-5">
-              <h3>{restaurant.name}</h3>
-              <p className = "text-sm text-mediumGray mb-2">{restaurant.location} | {restaurant.category}
-              </p>
-              <div className = "flex items-center">
-                <span className="flex mr-2">
-                  <FaStar className="text-lg text-secondary" />
-                  <FaStar className="text-lg text-secondary" />
-                  <FaStar className="text-lg text-secondary" />
-                  <FaStar className="text-lg text-secondary" />
-                  <FaStar className="text-lg text-secondary" />
-                </span>
-                <p className>{averageRating.toFixed(1)}</p>
-              </div>
+    <header className="flex-col bg-white mb-3">
+      <div className="flex">
+        <div>
+          <img src={restaurant.imgLink} alt="error" className="h-72 w-screen" />
+          <div className="p-5">
+            <h3>{restaurant.name}</h3>
+            <p className="text-sm text-mediumGray mb-2">
+              {restaurant.location} | {restaurant.category}
+            </p>
+            <div className="flex items-center">
+              <span className="flex mr-2">
+                <FaStar className="text-lg text-secondary" />
+                <FaStar className="text-lg text-secondary" />
+                <FaStar className="text-lg text-secondary" />
+                <FaStar className="text-lg text-secondary" />
+                <FaStar className="text-lg text-secondary" />
+              </span>
+              <p>{averageRating.toFixed(1)}</p>
             </div>
-            <hr/>
           </div>
+          <hr />
         </div>
-        <div className = "flex justify-between p-5">
-          <div className = "flex flex-col items-center">
-            <CiBookmark className = "text-2xl"/>
-            <p>저장</p>
-          </div>
-          <div className = "flex flex-col items-center">
-            <PiNotePencil className = "text-2xl"/>
-            <p>평가하기</p>
-          </div>
-          <div className = "flex flex-col items-center">
-            <CiLocationOn className = "text-2xl"/>
-            <p>체크인</p>
-          </div>
-          <div className = "flex flex-col items-center">
-            <RiShareForward2Fill className = "text-2xl"/>
-            <p>공유</p>
-          </div>
-          <div className = "flex flex-col items-center">
-            <CiFaceFrown className = "text-2xl"/>
-            <p>별로에요</p>
-          </div>
+      </div>
+      <div className="flex justify-between p-5">
+        <div className="flex flex-col items-center">
+          <CiBookmark className="text-2xl" />
+          <p>저장</p>
         </div>
-      </header>
-  )
+        <div className="flex flex-col items-center">
+          <PiNotePencil className="text-2xl" />
+          <p>평가하기</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <CiLocationOn className="text-2xl" />
+          <p>체크인</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <RiShareForward2Fill className="text-2xl" />
+          <p>공유</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <CiFaceFrown className="text-2xl" />
+          <p>별로에요</p>
+        </div>
+      </div>
+    </header>
+  );
 }
